e9e5115d948573159e2167e6d88842d01689c0f4
Chris Claude <christ.tchambila@gmail.com>
2021-11-18 19:22:58 +0200

    updated shop controller

--- api/shop/controllers/shop.js
+++ api/shop/controllers/shop.js
@@ -1,8 +1,96 @@
 'use strict';
+const {parseMultipartData, sanitizeEntity} = require('strapi-utils');
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  /**
+   * Create a shop.
+   *
+   * @return {Object}
+   */
+
+  async create(ctx) {
+    let entity;
+    if (ctx.is('multipart')) {
+      const {data, files} = parseMultipartData(ctx);
+      data.owner = ctx.state.user.id;
+      entity = await strapi.services.shop.create(data, {files});
+    } else {
+      ctx.request.body.owner = ctx.state.user.id;
+      entity = await strapi.services.shop.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, {model: strapi.models.shop});
+  },
+
+  /**
+   * Update a record.
+   *
+   * @return {Object}
+   */
+  async update(ctx) {
+    const {id} = ctx.params;
+
+    let entity;
+
+    const [shop] = await strapi.services.shop.find({
+      id: ctx.params.id,
+      'owner.id': ctx.state.user.id,
+    });
+
+    if (!shop) {
+      return ctx.unauthorized(`You can't update this entry`);
+    }
+
+    if (ctx.is('multipart')) {
+      const {data, files} = parseMultipartData(ctx);
+      entity = await strapi.services.shop.update({id}, data, {
+        files,
+      });
+    } else {
+      entity = await strapi.services.shop.update({id}, ctx.request.body);
+    }
+
+    return sanitizeEntity(entity, {model: strapi.models.shop});
+  },
+
+  async delete(ctx) {
+    const {id} = ctx.params;
+
+
+    const [shop] = await strapi.services.shop.find({
+      id: ctx.params.id,
+      'owner.id': ctx.state.user.id,
+    });
+
+    if (!shop) {
+      return ctx.unauthorized(`You can't update this entry`);
+    }
+
+    const entity = await strapi.services.shop.delete({id});
+
+    return sanitizeEntity(entity, {model: strapi.models.shop});
+  },
+
+  // Get logged in users shops
+  /*async me(ctx) {
+    const owner = ctx.state.user;
+
+    if (!owner) {
+      return ctx.badRequest(null, [
+        { messages: [{ id: "No authorization header was found" }] },
+      ]);
+    }
+
+    const data = await strapi.services.shop.find({ owner: owner.id });
+
+    if (!data) {
+      return ctx.notFound();
+    }
+
+    return sanitizeEntity(data, { model: strapi.models.shop });
+  },*/
+};
