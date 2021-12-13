5e41c2b09c07f8792f9248263c87f2990837c45c
Chris Claude <christ.tchambila@gmail.com>
2021-12-13 18:48:10 +0200

    updated dev db to postgres

--- config/database.js
+++ config/database.js
@@ -4,8 +4,13 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: "postgres",
+        host: env("DATABASE_HOST", "localhost"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "agroelikia-dev-db"),
+        username: env("DATABASE_USERNAME", "postgres"),
+        password: env("DATABASE_PASSWORD", "postgres"),
+        schema: env("DATABASE_SCHEMA", "public"),
       },
       options: {
         useNullAsDefault: true,
