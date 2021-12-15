c49c664db5c6267bd43a462bc3a1498ea88ef9ec
Chris Claude <christ.tchambila@gmail.com>
2021-12-15 14:34:43 +0200

    updated prod db config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,25 +1,6 @@
 const parse = require('pg-connection-string').parse;
 const config = parse(process.env.DATABASE_URL);
 
-console.log(config);
-
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: 'postgres',
-//     connection: {
-//       host: config.host,
-//       port: parseInt(config.port),
-//       database: config.database,
-//       username: config.user,
-//       password: config.password,
-//       ssl: {
-//         rejectUnauthorized: false
-//       },
-//     },
-//     debug: false,
-//   },
-// });
-
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
