fc0fdb1a63a28a1aa514289770757ac01c633a15
Chris Claude <christ.tchambila@gmail.com>
2021-12-15 14:01:05 +0200

    update ssl config in prod db config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -3,19 +3,40 @@ const config = parse(process.env.DATABASE_URL);
 
 console.log(config);
 
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'postgres',
+//     connection: {
+//       host: config.host,
+//       port: parseInt(config.port),
+//       database: config.database,
+//       username: config.user,
+//       password: config.password,
+//       ssl: {
+//         rejectUnauthorized: false
+//       },
+//     },
+//     debug: false,
+//   },
+// });
+
 module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: config.host,
-      port: parseInt(config.port),
-      database: config.database,
-      username: config.user,
-      password: config.password,
-      ssl: {
-        rejectUnauthorized: false
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: "postgres",
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
+      },
+      options: {
+        ssl: false,
       },
     },
-    debug: false,
   },
 });
+
