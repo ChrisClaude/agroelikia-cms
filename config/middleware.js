c6c092d4beeba6027a9790ba022d76f09c1764af
Chris Claude <christ.tchambila@gmail.com>
2021-12-23 23:44:06 +0200

    added cors url

--- config/middleware.js
+++ config/middleware.js
@@ -1,8 +1,17 @@
-module.exports = ({env}) => ({
+/*module.exports = ({env}) => ({
   settings: {
     cors: {
       enabled: true,
       origin: env('CORS_ORIGIN', 'https://agroelikia-frontend-app.vercel.app/').split(',')
     }
   }
-})
+})*/
+
+module.exports = {
+  settings: {
+    cors: {
+      enabled: true,
+      origin: ["https://agroelikia-frontend-app.vercel.app",],
+    },
+  },
+};
