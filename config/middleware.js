e847cd9265c6401b84c995e74c1589cb3fe497a2
Chris Claude <christ.tchambila@gmail.com>
2021-12-24 20:01:14 +0200

    updated middleware

--- config/middleware.js
+++ config/middleware.js
@@ -3,7 +3,7 @@ module.exports = ({env}) => ({
     cors: {
       enabled: true,
       headers: '*',
-      origin: ["https://agroelikia-frontend-app.vercel.app", "http://localhost:4200"]
+      origin: ["https://agroelikia-frontend-app.vercel.app", "http://localhost:4200", "http://localhost:1337", "https://peaceful-ravine-69370.herokuapp.com"]
     }
   }
 })
