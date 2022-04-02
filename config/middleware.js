3de3b2bd8bffaac73eca1881cb13d23a33c1cea6
Claude De-Tchambila <claude.detchambila@dariel.co.za>
2022-04-02 19:04:11 +0200

    Added agroelikia.com as one of the origins

--- config/middleware.js
+++ config/middleware.js
@@ -3,7 +3,14 @@ module.exports = ({env}) => ({
     cors: {
       enabled: true,
       headers: '*',
-      origin: ["https://agroelikia-frontend-app.vercel.app", "http://localhost:4200", "http://localhost:1337", "https://agroelikia.herokuapp.com"]
+      origin: [
+        "https://agroelikia-frontend-app.vercel.app",
+        "http://localhost:4200",
+        "http://localhost:1337",
+        "https://agroelikia.herokuapp.com",
+        "https://agroelikia.com",
+        "https://www.agroelikia.com",
+      ]
     }
   }
 })
